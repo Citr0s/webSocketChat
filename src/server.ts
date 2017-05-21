0c5b6ac5b9cb238c3b146acdbacde6eee3be561d
miloszmd <miloszdura@gmail.com>
2017-05-21 16:37:22 +0100

    Revert "moved connection handling login into connection class"
    This reverts commit 74371c691b7f4d016424b3ac275569f13286fa99.

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,49 @@
 import * as WebSocket from 'ws';
+import { DateFormatter } from "./Helpers/DateFormatter";
 import { Connection } from "./Server/Connection";
+import { Room } from "./Chat/Room";
 
 let connection = new Connection();
+let room = new Room();
 
 let WebSocketServer = WebSocket.Server;
 let wss = new WebSocketServer({port: 7253});
 
-wss.on('connection', connection.handle);
+wss.on('connection', function (ws) {
+
+    console.log('Client connected');
+
+    connection.addUser({
+        name: 'Unknown',
+        client: ws,
+        colour: ''
+    });
+
+    room.addUser(connection.getUser(ws));
+
+    ws.send(JSON.stringify(room.chat));
+
+    ws.on('message', function (message) {
+
+        let newMessage = {
+            date: new Date(),
+            message: message,
+            colour: connection.getUser(ws).colour
+        };
+
+        room.addMessage(newMessage);
+
+        console.log('[' + new DateFormatter(newMessage.date).toShortDate() + '] - ' + newMessage.message);
+
+        connection.broadcast(JSON.stringify(room.chat));
+    });
+
+    ws.on('close', function () {
+
+        console.log('Client disconnected');
+
+        connection.removeUser(ws);
+    });
+});
 
 console.log('WebSocket server listening for connection...');
