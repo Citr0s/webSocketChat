d514331dd6c9fa95a0136ab8af4340a456a47bc1
Milosz Dura <milosz.md@wp.pl>
2017-05-23 20:26:17 +0100

    improving logging during deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+echo 'Killing node process...';
 pkill -HUP node
+
+echo 'Getting latest changes from git...';
 git pull
+
+echo 'Updating packages...';
 yarn
+
+echo 'Compiling files...';
 gulp
+
+echo 'Starting node process...';
 node dist/server > stdout.txt 2> stderr.txt &
